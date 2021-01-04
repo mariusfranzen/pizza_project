efb7f212523c1d8132a0cb7bc3f4dc0efdc0cd46
Björn Malmgren <Anderion1984@gmail.com>
2021-01-04 13:25:32 +0100

    page index

---
+++ frontend/src/pages/public/index.js
@@ -0,0 +1,9 @@
+import AboutPage from "./AboutPage";
+import CheckoutPage from "./CheckoutPage";
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import MenuPage from "./MenuPage";
+import RegisterPage from "./RegisterPage";
+import UserPage from "./UserPage";
+
+export { AboutPage, MenuPage, RegisterPage, LoginPage, HomePage, CheckoutPage };
