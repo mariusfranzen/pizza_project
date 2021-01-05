abd263ae88d05e063711dc4d0551e7dba924b19c
Marius Franzén <marius.franzen1@gmail.com>
2021-01-05 10:29:25 +0100

    installed formik and added form component

---
+++ frontend/src/components/common/form/Form.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fields: []
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Form
