8d8d9a323affc028eadd4b6884e0c558a8b0a861
Marius Franzén <marius.franzen1@gmail.com>
2021-01-05 11:55:15 +0100

    created a functional form component

---
+++ frontend/src/components/common/Form.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fields: props.fields
+        }
+    }
+
+    render() {
+        const fields = this.state.fields.map((field) => 
+            <React.Fragment>
+                {field.label ? <label>{field.label}</label> : null}
+                <input type={field.type} id={field.id} name={field.id} className={field.className} placeholder={field.placeholder} />
+            </React.Fragment>
+        )
+
+        // TODO: add default style, that can be overriden
+
+        return (
+            <form>
+                {fields}
+            </form>
+        )
+    }
+}
+
+export default Form
