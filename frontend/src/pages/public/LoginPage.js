675e770c770ac92711aee8977fd0a75b0a0c14cf
Marius Franzén <marius.franzen1@gmail.com>
2021-01-07 11:37:50 +0100

    added login without db interaction

--- frontend/src/pages/public/LoginPage.js
+++ frontend/src/pages/public/LoginPage.js
@@ -1,13 +1,85 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Form from "../../components/common/Form";
+import UserApi from "../../apis/UserApi";
+import { useFormik } from "formik";
 
 export class LoginPage extends Component {
     render() {
         return (
-            <div>
-                <h1>LoginPage</h1>
-            </div>
-        )
+        <div>
+            <LoginForm />
+        </div>
+        );
     }
 }
 
-export default LoginPage
+const LoginForm = () => {
+    const validate = (values) => {
+        const errors = {};
+
+        if (!values.email) {
+            errors.email = "Required";
+        } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+        ) {
+            errors.email = "Invalid email address";
+        }
+
+        if (!values.password) {
+            errors.password = "Required";
+        }
+
+        //TODO: true if user was not found
+        if (false) {
+            errors.submit = "Email and/or password is incorrect";
+        }
+
+        return errors;
+    };
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validate,
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="email">Email: </label>
+            <input
+                id="email"
+                name="email"
+                type="text"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+            />
+            {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+
+            <label htmlFor="password">Password: </label>
+            <input
+                id="password"
+                name="password"
+                type="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+            />
+            {formik.errors.password ? (
+                <div>{formik.errors.password}</div>
+            ) : null}
+
+            <button id="submit" name="submit" type="submit">
+                Login
+            </button>
+            {formik.errors.submit ? <div>{formik.errors.submit}</div> : null}
+        </form>
+    );
+};
+
+export default LoginPage;
