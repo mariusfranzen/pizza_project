2023166a062744a972c8819e1a243c5076868ff7
Marius Franzén <marius.franzen1@gmail.com>
2021-01-15 11:55:29 +0100

    renamed menu to navmenu

---
+++ frontend/src/components/common/navbar/NavMenu.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+export class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: props.isOpen
+        }
+    }
+
+    shouldComponentUpdate(nextProps) {
+        return nextProps.isOpen !== this.state.isOpen;
+    }
+
+    componentDidUpdate(props) {
+        this.setState({isOpen: props.isOpen})
+        this.render();
+    }
+
+    render() {
+        let isLoggedIn = false;
+        return (
+            <div className="navMenu" style={this.state.isOpen ? {transform: "translateX(0%)"} : {transform: "translateX(100%)"}}>
+                <div className="topMenu">
+                    <a href="/">Hem</a>
+                    <a href="/menu">Meny</a>
+                    <a href="/about">Information</a>
+                    <a href="/contact">Kontakt</a>
+                </div>
+                <div className="bottomMenu">
+                    {isLoggedIn ? 
+                    <>
+                        <a href="/login">Logga in</a>
+                        <a href="/register">Registrera</a>
+                    </>
+                    :
+                    <>
+                        <a href="/profile">Din profil</a>
+                        <a href="/logout">Logga ut</a>
+                    </>}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Menu
