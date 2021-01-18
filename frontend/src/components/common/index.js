4b2b3d4cd051c2fcb3d3616f7836c2037ddace12
Björn Malmgren <Anderion1984@gmail.com>
2021-01-18 14:14:44 +0100

    smallstuff

--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -1,8 +1,9 @@
-
+import HomeInfo from "./HomeInfo";
 import Footer from "./Footer";
 import Navbar from "./navbar/Burger";
 import MenuItem from "./MenuItem"
 import PaymentForm from "./PaymentForm";
+import Button from "./Button";
 
-export { MenuItem, Navbar, Footer, PaymentForm }
+export { MenuItem, Navbar, Footer, PaymentForm, HomeInfo, Button }
 
