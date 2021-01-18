1c2945843de35ea851de38a59848ddf8b4e68b59
abdou <karimsalimkarim123@gmail.com>
2021-01-18 15:04:18 +0100

    Revert "ok"
    This reverts commit 3852f3dca40434f051c33e22d4f9a8fb892be7f4.

--- frontend/src/pages/public/UserPage.js
+++ frontend/src/pages/public/UserPage.js
@@ -179,10 +179,18 @@ const EditUser = (props) => {
                     />
                     {props.errors.adress ? <div className='errors'>{props.errors.adress}</div> : null}
 
+<<<<<<< HEAD
+                    <label htmlFor="oldPassword" >Old Password: </label>
+                    <Field
+                        className='Field'
+                        id="oldPassword"
+                        name="oldPassword"
+=======
                     <label htmlFor="password">Old Password: </label>
                     <Field
                         id="password"
                         name="password"
+>>>>>>> 6d1f46a11624a3511f92977803ead8d90f7aeb5d
                         type="password"
                         onChange={props.handleChange}
                         onBlur={props.handleBlur}
@@ -231,6 +239,13 @@ const EditUser = (props) => {
                         onBlur={props.handleBlur}
                         value={props.values.dateOfBirth}
                     />
+<<<<<<< HEAD
+                    </div>
+                    <button id="submit" name="submit" type="submit">
+                        Update Account
+                    </button>
+                    {props.errors.submit ? <div className='errors'>{props.errors.submit}</div> : null}
+=======
 
                     <label htmlFor="adress">Adress: </label>
                     <Field
@@ -248,7 +263,7 @@ const EditUser = (props) => {
                     {props.errors.submit ? (
                         <div>{props.errors.submit}</div>
                     ) : null}
-                    
+>>>>>>> 6d1f46a11624a3511f92977803ead8d90f7aeb5d
                 </Form>
             </div>
             
