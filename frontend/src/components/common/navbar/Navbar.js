49e8a95f31f3d9513a55a8983e3eeaf77e8c4a16
Björn Malmgren <Anderion1984@gmail.com>
2021-01-26 13:14:50 +0100

    homeknapp

--- frontend/src/components/common/navbar/Navbar.js
+++ frontend/src/components/common/navbar/Navbar.js
@@ -10,7 +10,7 @@ import useToggle from '../../hooks/useToggle';
 const cookies = new Cookies();
 
 async function validateJwtCookie(jwt) {
-    await UserApi.validateJwt(jwt);
+	await UserApi.validateJwt(jwt);
 }
 
 function Navbar() {
@@ -19,31 +19,34 @@ function Navbar() {
 	const [isLoggedIn, setLoggedIn] = useState(false);
 	const [isAdmin, setAdmin] = useState(false);
 	const [cookie, setCookie] = useState(null);
-    const [isOpen, toggleOpen] = useToggle();
+	const [isOpen, toggleOpen] = useToggle();
 	useEffect(() => {
 		if (cookie) {
-            let authCookie = cookies.get("auth");
-            let val = validateJwtCookie(authCookie);
-            setCookie(val);
-            if (val.data.authorization === "USER") {
-                setLoggedIn(true);
-            } else if (val.data.authorization === ("ADMIN" || "OWNER")) {
-                setAdmin(true);
-            } else {
-                setLoggedIn(false);
-                setAdmin(false);
-            }
+			let authCookie = cookies.get("auth");
+			let val = validateJwtCookie(authCookie);
+			setCookie(val);
+			if (val.data.authorization === "USER") {
+				setLoggedIn(true);
+			} else if (val.data.authorization === ("ADMIN" || "OWNER")) {
+				setAdmin(true);
+			} else {
+				setLoggedIn(false);
+				setAdmin(false);
+			}
 		}
-		
+
 	}, [cookie, isOpen]);
 
 	function cartClick() {
 		history.push("/checkout");
-    }
+	}
+	function homeClick() {
+		history.push("/");
+	}
 
 	return (
 		<nav className="mainNav">
-			<img src={Logo} alt="logo" className="logo" />
+			<img src={Logo} alt="logo" className="logo" onClick={homeClick} />
 			<nav className="rightGroup">
 				<img src={Icons8ShoppingCart} alt="cart" className="cart" onClick={cartClick} />
 				<NavBurger isOpen={isOpen} isLoggedIn={isLoggedIn} menuClick={toggleOpen} />
