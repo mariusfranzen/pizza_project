8cb4759e0d6b7f351b59196caf42598399315d63
Marius Franzén <marius.franzen1@gmail.com>
2021-01-27 15:26:26 +0100

    initialValues fix

--- frontend/src/pages/public/UserPage.js
+++ frontend/src/pages/public/UserPage.js
@@ -1,32 +1,9 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import UserApi from "../../apis/UserApi";
 import { Formik, Field, Form } from "formik";
+import Cookie from 'universal-cookie';
 
-export class UserPage extends Component {
-    constructor() {
-        super();
-        this.state = {
-            initialUser: {},
-        };
-    }
-    async componentDidMount() {
-        console.log("asd");
-        await UserApi.getUserById("5ffeb12a95a55750db3378a5").then(
-            async (result) => {
-                await this.setState({ initialUser: result.data });
-                console.log(result);
-            }
-        );
-    }
-
-    render() {
-        return (
-            <div className="form">
-                <EditUser initialUser={this.state.initialUser} />
-            </div>
-        );
-    }
-}
+const cookies = new Cookie();
 
 const validate = (values) => {
     const errors = {};
@@ -77,6 +54,194 @@ const validate = (values) => {
     }
     return errors;
 };
+
+function UserPage() {
+
+    const [user, setUser] = useState("");
+    useEffect(() => {
+        async function getUser() {
+            let lUser = (await UserApi.validateJwt(cookies.get("auth"))).data;
+            console.log(lUser)
+            setUser(lUser);
+        }
+        getUser();
+    }, [])
+    return (
+        <Formik
+            enableReinitialize
+            initialValues={{
+                email: user.email,
+                password: "",
+                newPassword: "",
+                passwordAgain: "",
+                phoneNumber: user.phoneNumber,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                dateOfBirth: user.dateOfBirth,
+                adress: user.adress,
+            }}
+            onSubmit={(values) => {
+                let user = {
+                    email: values.email,
+                    password: values.newPassword,
+                    phoneNumber: values.phoneNumber.replace(/[^0-9]$/, ""),
+                    firstName: values.firstName,
+                    lastName: values.lastName,
+                    dateOfBirth: values.dateOfBirth,
+                    adress: values.adress,
+                };
+                UserApi.updateUser(user);
+            }}
+            validate={validate}
+        >
+            {(props) => (
+
+                <div className='userpage'>
+                    <Form>
+
+                        <div className='div1'>
+
+                            <label htmlFor="firstName" >First Name: </label>
+                            <Field
+                                className='Field'
+                                id="firstName"
+                                name="firstName"
+                                type="text"
+                                placeholder="First Name"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.firstName}
+                            />
+                            {props.errors.firstName ? <div className='errors'>{props.errors.firstName}</div> : null}
+
+                            <label htmlFor="lastName" >Last Name: </label>
+                            <Field
+                                className='Field'
+                                id="lastName"
+                                name="lastName"
+                                type="text"
+                                placeholder="Last Name"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.lastName}
+                            />
+                            {props.errors.lastName ? <div className='errors'>{props.errors.lastName}</div> : null}
+
+                            <label htmlFor="phoneNumber" >Phone number: </label>
+                            <Field
+                                className='Field'
+                                id="phoneNumber"
+                                name="phoneNumber"
+                                type="text"
+                                placeholder="Phonenumber"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.phoneNumber}
+                            />
+                            {props.errors.phoneNumber ? (
+                                <div className='errors'>{props.errors.phoneNumber}</div>
+                            ) : null}
+
+                            <label htmlFor="email" >Email: </label>
+                            <Field
+                                className='Field'
+                                id="email"
+                                name="email"
+                                type="text"
+                                placeholder="E-mail"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.email}
+                            />
+                            {props.errors.email ? <div className='errors'>{props.errors.email}</div> : null}
+                        </div>
+
+                        <div className='div2'>
+
+                            <label htmlFor="adress" >Adress: </label>
+                            <Field
+                                className='Field'
+                                id="adress"
+                                name="adress"
+                                type="text"
+                                placeholder="Adress"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.adress}
+                            />
+                            {props.errors.adress ? <div className='errors'>{props.errors.adress}</div> : null}
+
+                            <label htmlFor="password">Old Password: </label>
+                            <Field
+
+                                className='Field'
+                                id="password"
+                                name="password"
+                                type="password"
+                                placeholder="Old Password"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.password}
+                            />
+                            {props.errors.password ? (
+                                <div className='errors'>{props.errors.password}</div>
+                            ) : null}
+
+                            <label htmlFor="newPassword" >New Password: </label>
+                            <Field
+                                className='Field'
+                                id="newPassword"
+                                name="newPassword"
+                                type="password"
+                                placeholder="New Password"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.newPassword}
+                            />
+                            {props.errors.newPassword ? (
+                                <div className='errors'>{props.errors.newPassword}</div>
+                            ) : null}
+
+                            <label htmlFor="passwordAgain" >New Password again: </label>
+                            <Field
+                                className='Field'
+                                id="passwordAgain"
+                                name="passwordAgain"
+                                type="password"
+                                placeholder="Repeat New Password"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.passwordAgain}
+                            />
+                            {props.errors.passwordAgain ? (
+                                <div className='errors'>{props.errors.passwordAgain}</div>
+                            ) : null}
+
+                            <label htmlFor="dateOfBirth">Date of Birth: </label>
+                            <Field
+                                className='Field'
+                                id="dateOfBirth"
+                                name="dateOfBirth"
+                                type="date"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.dateOfBirth}
+                            />
+
+                        </div>
+                        <button id="submit" className="button" name="submit" type="submit">
+                            Edit profile
+                             </button>
+                        {props.errors.submit ? (
+                            <div>{props.errors.submit}</div>
+                        ) : null}
+                    </Form>
+                </div>
+
+            )}
+        </Formik>
+    );
+}
 
 const EditUser = (props) => {
     return (
